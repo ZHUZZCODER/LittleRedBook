5a2a609215e881941d4c52802d0b48365f91fc53
ZHUZIZHEN0509 <2337658094@qq.com>
2024-01-11 15:42:42 +0800

    feat: react-native-config集成环境变量

--- src/views/welcome/index.tsx
+++ src/views/welcome/index.tsx
@@ -1,6 +1,7 @@
 import React, {memo} from 'react';
 import type {FC, ReactNode} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import {API_URL} from '@/config';
 
 interface IProps {
   children?: ReactNode;
@@ -9,7 +10,7 @@ interface IProps {
 const Welcome: FC<IProps> = props => {
   return (
     <View style={styles.container}>
-      <Text>Welcome</Text>
+      <Text>Welcome1{API_URL}</Text>
     </View>
   );
 };
